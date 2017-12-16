4930adf7ddb0422664769d7ab5c8e7a545d89bf0
Ross <ross@tests-MacBook-Pro.local>
2017-12-16 15:35:23 -0600

    hard mode

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-console.log('console');
+console.log('In console');
 
 $(document).ready(start);
 
@@ -6,6 +6,7 @@ function start(){
   console.log('In DOM');
   $('#inputSubmit').on('click', submitEmployee);
   $('#inputSubmit').on('click', computeSalary);
+  $('table').on('click', '.delete', removeEmployee);
 } // end DOM start
 
 var employees =[];
@@ -18,7 +19,6 @@ class Employee{
     this.jobTitle = jobTitle;
     this.annualSalary = annualSalary;
   } // end Employee object constructor
-
 } // end Employee Class
 
 function computeSalary(){
@@ -28,20 +28,27 @@ function computeSalary(){
     console.log(totalSalary);
     $('footer').children('h3').text('$' + Math.round(totalSalary/12));
   } // end for loop
-
 } // end computeSalary
 
 function submitEmployee(){
   var employee = new Employee($('#inputFirstName').val(),$('#inputLastName').val(),$('#inputIdNumber').val(),$('#inputJobTitle').val(),$('#inputAnnualSalary').val());
-  console.log(employee);
+    console.log(employee);
   employees.push(employee);
-  console.log(employees);
+    console.log(employees);
   var $row = $('<tr>');
     $row.append('<td>' + employee.firstName + '</td>');
     $row.append('<td>' + employee.lastName + '</td>');
     $row.append('<td>' + employee.idNumber + '</td>');
     $row.append('<td>' + employee.jobTitle + '</td>');
     $row.append('<td>' + '$' + employee.annualSalary + '</td>');
+    $row.append('<td><button class ="delete">Delete</button></td>');
     $('#employeeInfo').append($row);
     $('input').val('');
-}
+} // end submitEmployee function
+
+function removeEmployee(){
+
+  $(this).closest('tr').remove();
+
+  computeSalary();
+} // end removeEmployee function
